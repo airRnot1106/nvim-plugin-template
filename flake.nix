ef382cb2dcfef10bf0fd4b73ec0a166aec75cb02
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-06-16 23:11:14 +0900

    chore(nix): add git hooks and treefmt configuration

--- flake.nix
+++ flake.nix
@@ -1,15 +1,25 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     # https://github.com/NixOS/nixpkgs/pull/531947
     nixpkgs.url = "github:NixOS/nixpkgs/57e69b6f17cf4d4ad4ed90a31a3b21aa1197d824";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
+      self,
       flake-utils,
+      git-hooks,
       nixpkgs,
-      ...
+      treefmt-nix,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -66,16 +76,20 @@
         };
       in
       {
-        devShells = {
-          default = pkgs.mkShellNoCC {
-            packages = (
-              with pkgs;
-              [
-                neovim
-              ]
-            );
+        devShells =
+          let
+            inherit (self.checks.${system}.pre-commit) shellHook enabledPackages;
+          in
+          {
+            default = pkgs.mkShellNoCC {
+              inherit shellHook;
+              packages =
+                (with pkgs; [
+                  neovim
+                ])
+                ++ enabledPackages;
+            };
           };
-        };
 
         packages = {
           default = plugin;
@@ -86,6 +100,20 @@
           vhs = flake-utils.lib.mkApp { drv = vhs-script; } // {
             meta.description = "Run the demo script with vhs";
           };
+        };
+
+        formatter =
+          let
+            treefmtEval = treefmt-nix.lib.evalModule pkgs ./nix/treefmt.nix;
+          in
+          treefmtEval.config.build.wrapper;
+
+        checks = {
+          pre-commit = git-hooks.lib.${system}.run (
+            import ./nix/git-hooks.nix {
+              inherit self pkgs;
+            }
+          );
         };
       }
     );
